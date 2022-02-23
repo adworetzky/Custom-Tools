7ac7e5450bfe7aca089834d29294cf4b401e2716
adworetzky <adworetzky@mica.edu>
2022-02-23 11:17:14 -0500

    path change, styling

--- svgeditor/scripts.js
+++ svgeditor/scripts.js
@@ -7,7 +7,7 @@
 // 3c. Redraw SVG
 
 const canvasId = 'canvas0',
-  defaultSvg = '/assets/sampleSVG.svg',
+  defaultSvg = './assets/sampleSVG.svg',
   inputId = 'text-entry-box',
   objId = 'svg-object',
   textId = 'textID';
@@ -15,7 +15,7 @@ const canvasId = 'canvas0',
 const init = () => {
   // Sidebar
   const sidebar = document.createElement('div');
-  sidebar.id='ui-wrapper';
+  sidebar.id = 'ui-wrapper';
   const label = document.createElement('label');
   label.for = inputId;
   label.className = 'ui-element';
@@ -30,7 +30,7 @@ const init = () => {
   const canvasWrapper = document.createElement('div');
   canvasWrapper.id = 'canvas-wrapper';
   const canvas = document.createElement('canvas');
-  canvas.id  = canvasId;
+  canvas.id = canvasId;
   canvas.width = 1080;
   canvas.height = 1080;
   canvasWrapper.appendChild(canvas);
@@ -47,45 +47,48 @@ const init = () => {
   document.querySelector('body').appendChild(obj);
 
   updateDrawing.init();
-}
+};
 
 const updateDrawing = {
-	init: function() {
+  init: function () {
     const svg = document.getElementById(objId);
-    
+
     // wait for this to fully load, then update the canvas element
     svg.addEventListener('load', () => {
       updateDrawing.updateCanvas(svg.contentDocument.querySelector('svg'));
     });
     updateDrawing.listener();
   },
-	listener: function() {
+  listener: function () {
     const inputEl = document.getElementById(inputId);
-		inputEl.addEventListener( 'keyup', () => {
+    inputEl.addEventListener('keyup', () => {
       // console.log(inputEl.value);
       updateDrawing.updateSvg(inputEl.value);
-    } );
-	},
-  updateSvg: function(newText) {
+    });
+  },
+  updateSvg: function (newText) {
     const svg = document.getElementById(objId).contentDocument;
     const text = svg.getElementById(textId);
     // this replaces the existing text with whatever is passed into to the updateDrawing.updateSvg function
     text.textContent = newText;
     // and then updates the canvas to match
     updateDrawing.updateCanvas(svg.querySelector('svg'));
-
   },
-  updateCanvas: function(svg) {
+  updateCanvas: function (svg) {
     svg = makeBlobFromSVG(svg);
     let c0 = document.getElementById(canvasId);
-    let ctx0 = c0.getContext("2d");
+    let ctx0 = c0.getContext('2d');
     const image = new Image();
-    image.addEventListener('load', () => {
-      ctx0.drawImage(image, 0, 0, c0.width, c0.height);
-    }, false);
+    image.addEventListener(
+      'load',
+      () => {
+        ctx0.drawImage(image, 0, 0, c0.width, c0.height);
+      },
+      false
+    );
     image.src = svg;
-  }
-}
+  },
+};
 
 // Generate a Blob URL to later use as image.src to load the svg as an imag to the canvas
 function makeBlobFromSVG(svg) {
