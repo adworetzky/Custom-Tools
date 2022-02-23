6a61dab2ad68aba07e8a482966fdedbd867a7e57
Angelique Weger <hello@angeliqueweger.com>
2022-02-23 00:15:33 -0500

    Recreate UI w/o jquery

--- svgeditor/scripts.js
+++ svgeditor/scripts.js
@@ -5,3 +5,46 @@
 // 3a. Create event listener
 // 3b. Update SVG
 // 3c. Redraw SVG
+
+const canvasId = 'canvas0',
+  defaultSvg = '/assets/sampleSVG.svg',
+  inputId = 'text-entry-box',
+  objId = 'svg-object',
+  textId = 'textID';
+
+const init = () => {
+  // Sidebar
+  const sidebar = document.createElement('div');
+  sidebar.id='ui-wrapper';
+  const label = document.createElement('label');
+  label.for = inputId;
+  label.className = 'ui-element';
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.id = inputId;
+  input.className = 'ui-element';
+  sidebar.appendChild(label);
+  sidebar.appendChild(input);
+
+  // Canvas
+  const canvasWrapper = document.createElement('div');
+  canvasWrapper.id = 'canvas-wrapper';
+  const canvas = document.createElement('canvas');
+  canvas.id  = canvasId;
+  canvas.width = 1080;
+  canvas.height = 1080;
+  canvasWrapper.appendChild(canvas);
+
+  // Object
+  const obj = document.createElement('object');
+  obj.id = objId;
+  obj.type = 'image/svg+xml';
+  obj.data = defaultSvg;
+
+  // Update DOM
+  document.querySelector('main').appendChild(sidebar);
+  document.querySelector('main').appendChild(canvasWrapper);
+  document.querySelector('body').appendChild(obj);
+}
+
+window.onload = init();
