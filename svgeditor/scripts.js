8ec8bf187e37e82dea890e40c50be2f16a1e75c5
adworetzky <adworetzky@mica.edu>
2022-02-23 19:51:13 -0500

    Angelique changes with my edits too

--- svgeditor/scripts.js
+++ svgeditor/scripts.js
@@ -1,86 +1,104 @@
-// A SVG editor boiler plate for use as a template interface. Basically a non-designer need to be able to edit the text of SVGs we make in figma and export them as pngs to post places.
+// Order of operations
+// 1. Create UI
+// 2. Draw first SVG
+// 3. Event listener actions:
+// 3a. Create event listener
+// 3b. Update SVG
+// 3c. Redraw SVG
 
-setup();
+const canvasId = 'canvas0',
+  defaultSvg = './assets/sampleSVG.svg',
+  inputId = 'text-entry-box',
+  objId = 'svg-object',
+  textId = 'textID';
 
-// Main OOP
-$(window).on("load", function () {
-  //for Angelique: I'm having a lot of trouble with the order of things and what to put event listeners on.
-  // I'm fairly sure the order of things is:
+const init = () => {
+  // Sidebar
+  const sidebar = document.createElement('div');
+  sidebar.id = 'ui-wrapper';
+  const tiWrapper = document.createElement('div');
+  tiWrapper.id = 'ti-wrapper';
+  const label = document.createElement('label');
+  label.for = inputId;
+  label.className = 'ui-element';
+  label.innerHTML = 'Text Input';
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.id = inputId;
+  input.className = 'ui-element';
+  sidebar.appendChild(tiWrapper);
+  tiWrapper.appendChild(label);
+  tiWrapper.appendChild(input);
 
-  // get the content of the SVG object
-  let svgObject = document.getElementById("svg-object").contentDocument;
-  console.log(svgObject);
+  // Canvas
+  const canvasWrapper = document.createElement('div');
+  canvasWrapper.id = 'canvas-wrapper';
+  const canvas = document.createElement('canvas');
+  canvas.id = canvasId;
+  canvas.width = 1080;
+  canvas.height = 1080;
+  canvasWrapper.appendChild(canvas);
 
-  // create an image that later I can change the src of
-  let img = $("<img id=svg-img>");
-  img.appendTo("body");
+  // Object
+  const obj = document.createElement('object');
+  obj.id = objId;
+  obj.type = 'image/svg+xml';
+  obj.data = defaultSvg;
 
-  // Make a change to the original SVG
-  changeTextofSVG(svgObject, document.querySelector("#text-entry-box").value);
-  $("#text-entry-box").on("input", function () {
-    changeTextofSVG(svgObject, document.querySelector("#text-entry-box").value);
-  });
+  // Update DOM
+  document.querySelector('main').appendChild(sidebar);
+  document.querySelector('main').appendChild(canvasWrapper);
+  document.querySelector('body').appendChild(obj);
 
-  // Generate Blob URL from SVG object
-  let url = makeBlobFromSVG(svgObject);
+  updateDrawing.init();
+};
 
-  // Draw an image to the Canvas
-  drawImageToCanvas(document.querySelector("#svg-img"));
+const updateDrawing = {
+  init: function () {
+    const svg = document.getElementById(objId);
 
-  // Update that image src as the blob URL
-  updateImageSrc(document.querySelector("#svg-img"), url);
-
-  // For the life of me, I can't get it to run. I can change an element of the original SVG. I think my main problem is an async one. I'm not sure when to have things trigger.
-});
-
-//Change the Text of my sample svg
-function changeTextofSVG(svgObject, string) {
-  svgObject.getElementById("textID").textContent = string;
-  console.log("Text Changed");
-}
+    // wait for this to fully load, then update the canvas element
+    svg.addEventListener('load', () => {
+      updateDrawing.updateCanvas(svg.contentDocument.querySelector('svg'));
+    });
+    updateDrawing.listener();
+  },
+  listener: function () {
+    const inputEl = document.getElementById(inputId);
+    inputEl.addEventListener('keyup', () => {
+      // console.log(inputEl.value);
+      updateDrawing.updateSvg(inputEl.value);
+    });
+  },
+  updateSvg: function (newText) {
+    const svg = document.getElementById(objId).contentDocument;
+    const text = svg.getElementById(textId);
+    // this replaces the existing text with whatever is passed into to the updateDrawing.updateSvg function
+    text.textContent = newText;
+    // and then updates the canvas to match
+    updateDrawing.updateCanvas(svg.querySelector('svg'));
+  },
+  updateCanvas: function (svg) {
+    svg = makeBlobFromSVG(svg);
+    let c0 = document.getElementById(canvasId);
+    let ctx0 = c0.getContext('2d');
+    const image = new Image();
+    image.addEventListener(
+      'load',
+      () => {
+        ctx0.drawImage(image, 0, 0, c0.width, c0.height);
+      },
+      false
+    );
+    image.src = svg;
+  },
+};
 
 // Generate a Blob URL to later use as image.src to load the svg as an imag to the canvas
-function makeBlobFromSVG(svgObject) {
-  const blob = new Blob([svgObject], { type: "image/svg+xml" });
+function makeBlobFromSVG(svg) {
+  const blob = new Blob([svg.outerHTML], { type: 'image/svg+xml' });
   const url = URL.createObjectURL(blob);
-  console.log(url);
   return url;
 }
 
-// draw the blob to the canvas
-function drawImageToCanvas(image) {
-  let c0 = document.querySelector("#canvas0");
-  let ctx0 = c0.getContext("2d");
-
-  $("#svg-img").on("load", function () {
-    ctx0.drawImage(image, 0, 0, c0.width, c0.height);
-  });
-  console.log("imageDrawn");
-}
-
-// update the "image" src
-function updateImageSrc(image, url) {
-  image.src = url;
-  console.log("src updated");
-}
-
-function setup() {
-  // Wrappers
-  $("<div id=ui-wrapper>").appendTo("main");
-  $("<div id=canvas-wrapper>").appendTo("main");
-  $("<div id=ti-wrapper>").appendTo("#ui-wrapper");
-
-  // Canvas
-  $("<canvas id=canvas0 width=1080 height=1080 >").appendTo("#canvas-wrapper");
-
-  // Inputs
-  $("<input type=text class=ui-element id=text-entry-box>").appendTo(
-    "#ti-wrapper"
-  );
-  $("#text-entry-box").before("<label class=ui-element>Text Input</label>");
-
-  // load svg into DOM so it can be accessed
-  $(
-    "<object id=svg-object data=assets/sampleSVG.svg type=image/svg+xml>"
-  ).appendTo("#ui-wrapper");
-}
+window.onload = init();
